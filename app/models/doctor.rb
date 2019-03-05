0f6155651a8e81430b32e719b903c062a93b976d
koen wille <koen.wille.kwa@gmail.com>
2019-03-05 16:13:25 +0100

    did gemfile, added some code in model and create koen doctor, now up to controller shit

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,8 @@
 class Doctor < ApplicationRecord
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   has_many :slots, dependent: :destroy
   has_many :bookings, through: :slots
   has_many :reviews, dependent: :destroy
