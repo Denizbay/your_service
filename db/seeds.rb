ce4bac86fdb26d9e5e59c3994376d724fe56cf32
Joost Dyckerhoff <joost.dy@web.de>
2019-03-05 13:43:43 +0100

    edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+fr = Language.create(name: 'french')
+en = Language.create(name: 'english')
+ger = Language.create(name: 'german')
+
+md = Field.create(name: 'MD')
+surgeon = Field.create(name: 'Surgeon')
+
+names_for_surgeons = [
+  "Dr. Stephan-Peter Hamm", "Dr. Susanne Lutz", "Dr. Christopher Marchand",
+  "Dr. Michael Hoffmann", "Dr. Med Susanne Schroeder" "Dr. Med Susann Schumann"
+]
+
+names_for_surgeons_emails =
+
+
+
+
+
+10.times do
+  doctor = Doctor.create(
+    name: names_for_surgeons.sample
+    email:
+    field: [md, surgeon].sample
+    language: [fr, en, ger].sample
+    description:
+
+
+
+    DoctorLanguage.create(doctor: doctor, language: fr)
+    10.times do
+      Slot.create()
+    end
+  end
+
+
+
+  puts 'Cleaning the database...'
+  Doctor.destroy_all
+
+  fake_attributes = [{},]
+
+
+  Doctor.create!(fake_attributes)
+  puts 'adding photos'
+  puts "Done!"
