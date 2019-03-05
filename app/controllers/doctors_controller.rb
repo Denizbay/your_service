817be8047d823013ef79dd9ec3574d8bc5d76dd7
koen wille <koen.wille.kwa@gmail.com>
2019-03-05 19:25:44 +0100

    created REAAAAAL addresses for the doctor sample, need to fix map

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,16 @@
+class DoctorsController < ApplicationController
+skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @doctors = Doctor.where.not(latitude: nil, longitude: nil)
+
+    @markers = @doctors.map do |doctor|
+      {
+        lng: doctor.longitude,
+        lat: doctor.latitude,
+        # infoWindow: render_to_string(partial: "infowindow", locals: { doctor: doctor })
+      }
+    end
+  end
+
+end
