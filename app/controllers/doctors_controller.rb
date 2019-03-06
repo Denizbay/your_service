a0027093a2f7980c36f63ab65e69552da4d937fa
koen wille <koen.wille.kwa@gmail.com>
2019-03-06 14:56:06 +0100

    map working added cards dimitri fixed the centered map

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,13 +2,15 @@ class DoctorsController < ApplicationController
 skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @doctors = Doctor.all
+
     @doctors = Doctor.where.not(latitude: nil, longitude: nil)
 
     @markers = @doctors.map do |doctor|
       {
         lng: doctor.longitude,
         lat: doctor.latitude,
-        # infoWindow: render_to_string(partial: "infowindow", locals: { doctor: doctor })
+        infoWindow: render_to_string(partial: "/doctors/info_window", locals: { doctor: doctor })
       }
     end
   end
